19a69eafeeed81451f49f3e41de2c1a7afd53bad
Dogl4 <doougllas@hotmail.com.br>
2021-07-09 21:26:14 -0300

    c9 - Adiciona funcionalidade de clicar duas vezes em um item, faz com que ele seja riscado, clicando duas vezes novamente no item desativa.

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const objEle = {
+const objEle = { // Elementos fixos
   botaoAdiciona: document.querySelector('#criar-tarefa'),
   inputAdiciona: document.querySelector('#texto-tarefa'),
   olLista: document.querySelector('#lista-tarefas'),
 };
 
-const objFun = {
-  eventoAdicionaLista: function adiciona() {
-    objEle.botaoAdiciona.addEventListener('click', () => { // Evento adiciona
+const objFun = { // Funções
+  eventoAdicionaLista: function adiciona() { // Evento adiciona li
+    objEle.botaoAdiciona.addEventListener('click', () => {
       const li = document.createElement('li');
       li.innerHTML = objEle.inputAdiciona.value;
       objEle.olLista.appendChild(li);
       objEle.inputAdiciona.value = '';
     });
   },
-  eventoLiCor: function eventoCor() {
+  eventoLiCor: function eventoCor() { // Evento colocar cor cinza da lista
     objEle.olLista.addEventListener('click', (e) => {
       const lista = document.querySelectorAll('li');
       for (let i = 0; i < lista.length; i += 1) {
@@ -29,9 +29,15 @@ const objFun = {
       }
     });
   },
+  riscaLista: function clickaDuasVezesRisca() {
+    objEle.olLista.addEventListener('dblclick', (e) => {
+      e.target.classList.toggle('completed');
+    });
+  }, // Referencias <https://dom.spec.whatwg.org/#ref-for-dom-element-getattribute%E2%91%A0 | https://developer.mozilla.org/pt-BR/docs/Web/API/Element/classList>
 };
 
 window.onload = () => {
   objFun.eventoAdicionaLista();
   objFun.eventoLiCor();
+  objFun.riscaLista();
 };
